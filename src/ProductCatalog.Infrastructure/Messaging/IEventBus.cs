424cd36b79d1e1edbfe157962bce28da024aef84
Angelo Pirola <angelo9871@gmail.com>
2026-03-17 08:52:28 +0100

    Add IEventBus interface for async domain event publishing
    Introduced IEventBus in ProductCatalog.Infrastructure.Messaging with a generic PublishAsync method to support asynchronous publishing of domain events implementing IDomainEvent. Imported ProductCatalog.Domain.Common for event type reference.

---
+++ src/ProductCatalog.Infrastructure/Messaging/IEventBus.cs
@@ -0,0 +1,8 @@
+﻿using ProductCatalog.Domain.Common;
+
+namespace ProductCatalog.Infrastructure.Messaging;
+
+public interface IEventBus
+{
+    Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default) where TEvent : IDomainEvent;
+}
