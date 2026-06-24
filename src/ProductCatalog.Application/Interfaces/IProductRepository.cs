d85e288f1623807368907bf44a29f0bb2cda2045
Angelo Pirola <angelo9871@gmail.com>
2026-06-24 17:51:01 +0200

    Add IProductRepository and IUnitOfWork interfaces
    Introduce IProductRepository for async CRUD operations on Product entities. Add IUnitOfWork interface to handle async save changes. Both interfaces are placed in the ProductCatalog.Application.Interfaces namespace.

---
+++ src/ProductCatalog.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Application.Interfaces;
+
+public interface IProductRepository
+{
+    Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<List<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task AddAsync(Product product, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Product product, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Product product, CancellationToken cancellationToken = default);
+}
